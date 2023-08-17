ce44dfaa4ff2238bb6dc49ebc459c8adf9d80501
Eduardo Ulises Martínez Vaca <81331013+EduardoMV@users.noreply.github.com>
2023-08-17 15:12:31 -0600

    V_1.0

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include <iostream>
 
 using namespace cv;
 using namespace std;
 
 int main() {
 
-    Mat A,B,C,D,E(500, 500, CV_8U, Scalar(0));
+    Mat A(500, 500, CV_8U, Scalar(0));
+    Mat B(500, 500, CV_8U, Scalar(0));
+    Mat C(500, 500, CV_8U, Scalar(0));
+    Mat D(500,500, CV_8U, Scalar(0));
+    Mat E(500,500, CV_8U, Scalar(0));
 
     //MatB
     for (int j = 255; j < 499; j++){
@@ -52,7 +55,22 @@ int main() {
     int squareSize = E.rows/5;
     for (int j = 0; j < E.rows; j++){
         for (int i = 0; i < E.cols; i++) {
-            for ()
+            if(j / squareSize % 2 == 0){
+                if(i / squareSize % 2 == 0){
+                    E.at<uchar>(j, i) = uchar(0);
+                }
+                else{
+                    E.at<uchar>(j, i) = uchar(255);
+                }
+            }
+            else{
+                if(i / squareSize % 2 == 1){
+                    E.at<uchar>(j, i) = uchar(0);
+                }
+                else{
+                    E.at<uchar>(j, i) = uchar(255);
+                }
+            }
         }
     }
 
